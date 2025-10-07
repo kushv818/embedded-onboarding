62b5d8f035884275de599289938353441afad4c3
Kush Vashishtha <kushvashishtha@Kushs-MacBook-Pro.local>
2025-10-06 20:53:15 -0500

    project files created

---
+++ section-01/work/memorytracker/inc/allocation.h
@@ -0,0 +1,8 @@
+#ifndef ALLOCATION_H
+#define ALLOCATION_H
+
+#include <stddef.h>
+
+
+
+#endif // ALLOCATION_H
---
+++ section-01/work/memorytracker/src/main.c
@@ -0,0 +1,20 @@
+int main(){
+
+//add 3 allocations
+
+//print all
+
+//remove one by id
+
+//print again
+
+//free all at the end
+
+/*
+Additional features:
+add lookup by id function
+automatically assign ids
+Add safety checks for double-free or invalid IDs
+*/
+    return 0;
+}
